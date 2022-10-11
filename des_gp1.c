cdd14fe46ac01c84d8b23c9cad757ad98e8e2757
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-10-11 19:22:46 +0200

    Initial
    des_gp1.c contains function definitions
    DES_TI.c contains TI's code with some comments

---
+++ des_gp1.c
@@ -0,0 +1,64 @@
+unsigned long long PermutedChoice1(unsigned long long key)
+{
+
+}
+
+unsigned long long LeftCircularShift(unsigned long long key)
+{
+
+}
+
+unsigned long long PermutedChoice2(unsigned long long key)
+{
+
+}
+
+unsigned long long InitialPermutation(unsigned long long text)
+{
+
+}
+
+unsigned long long SwapHalves(unsigned long long text)
+{
+
+}
+
+unsigned long long InverseInitialPermutation(unsigned long long text)
+{
+
+}
+
+unsigned long long ExpansionPermutation(unsigned long long text)
+{
+
+}
+
+unsigned long long SBox(unsigned long long text)
+{
+
+}
+
+unsigned long long Permutation(unsigned long long text)
+{
+
+}
+
+unsigned long long FeistelFunction(unsigned long long text)
+{
+
+}
+
+char *encrypt(char *text, char key)
+{
+
+}
+
+char *decrypt(char *cipher, char key)
+{
+    
+}
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
