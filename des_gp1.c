f3caaeab0259232229507ba50ea0516658af3da0
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-10-12 15:34:10 +0200

    Done

--- des_gp1.c
+++ des_gp1.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 unsigned long long PermutedChoice1(unsigned long long key)//Amin
 {
     
@@ -40,7 +42,16 @@ unsigned long long SBox(unsigned long long text)
 
 unsigned long long Permutation(unsigned long long text)
 {
-
+    static const int table[] = {16,  7, 20, 21, 29, 12, 28, 17,
+								1, 15, 23, 26, 5, 18, 31, 10,
+								2,  8, 24, 14, 32, 27,  3,  9,
+							    19, 13, 30,  6, 22, 11,  4, 25};
+    unsigned long long permuted = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        permuted |= ((text >> (32 - table[i])) & 0x01) << 31-i;
+    }
+    return permuted;
 }
 
 unsigned long long FeistelFunction(unsigned long long text, unsigned long long k)
@@ -60,5 +71,7 @@ char *decrypt(char *cipher, char key)
 
 int main(int argc, char* argv[])
 {
+    unsigned long long text = 0xA03538C2;
+    printf("%x", Permutation(text));
     return 0;
 }
