0f50a26ed2f125f8e2ca7c94fa81c082267e171d
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-10-12 16:21:11 +0200

    Done

--- des_gp1.c
+++ des_gp1.c
@@ -42,22 +42,43 @@ unsigned long long SBox(unsigned long long text)
 
 unsigned long long Permutation(unsigned long long text)
 {
-
+    static const int table[] = {16,  7, 20, 21, 29, 12, 28, 17,
+								1, 15, 23, 26, 5, 18, 31, 10,
+								2,  8, 24, 14, 32, 27,  3,  9,
+							    19, 13, 30,  6, 22, 11,  4, 25};
+    unsigned long long permuted = 0;
+    for (int i = 0; i < 32; i++)
+    {
+        permuted |= ((text >> (32 - table[i])) & 0x01) << 31-i;
+    }
+    return permuted;
 }
 
-unsigned long long FeistelFunction(unsigned long long text, unsigned long long k)
+unsigned long long FeistelFunction(unsigned long long text, unsigned long long key)
 {
-
+    return (Permutation(SBox(ExpansionPermutation(text & 0xFFFFFFFF) ^ key)) ^ (text >> 32)) | (text << 32);
 }
 
-char *encrypt(char *text, char key)
+unsigned long long encrypt(unsigned long long text, char key)
 {
-
+    generateKeys(key);
+    unsigned long long cipher = FesitelFunction(text, keys[0]);
+    for (int i = 1; i < 15; i++)
+    {
+        cipher = FeistelFunction(cipher, keys[i]);
+    }
+    return cipher;
 }
 
-char *decrypt(char *cipher, char key)
+unsigned long long decrypt(unsigned long long cipher, char key)
 {
-    
+    generateKeys(key);
+    unsigned long long text = FesitelFunction(cipher, keys[15]);
+    for (int i = 14; i >= 0; i--)
+    {
+        text = FeistelFunction(text, keys[i]);
+    }
+    return text;
 }
 
 void generateKeys(unsigned long long key)
