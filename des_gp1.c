51501e93ddfa92a6d4b536ef1ab9a0ca38dd258a
Ahmedkhalifa1999 <ahmedkhalifa1999@gmail.com>
2022-10-23 23:01:49 +0200

    Various Optimizations

--- des_gp1.c
+++ des_gp1.c
@@ -3,96 +3,98 @@
 #include <time.h>
 #include <pthread.h>
 
+#define BLOCK_SIZE 2048
+#define THREAD_COUNT 16
+
 #define PLAIN_TEXT_SIZE	64
 #define MUSK 0x8000000000000000
 
 unsigned long long keys[16] = {0};
 
-int S1[4][16] = { {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
-                {0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
-                {4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
-                {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13} };
-int S2[4][16] = { {15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10},
-                {3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5},
-                {0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15 },
-                {13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9} };
+const unsigned char S1[4][16] = { {14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
+                                  {0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
+                                  {4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
+                                  {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13} };
+const unsigned char S2[4][16] = { {15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10},
+                                  {3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5},
+                                  {0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15 },
+                                  {13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9} };
 
-int S3[4][16] = { {10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8},
-                {13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1},
-                {13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7},
-                {1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12 } };
+const unsigned char S3[4][16] = { {10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8},
+                                  {13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1},
+                                  {13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7},
+                                  {1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12 } };
 
-int S4[4][16] = { { 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15},
-                {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
-                {10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4},
-                {3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14 } };
+const unsigned char S4[4][16] = { { 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15},
+                                  {13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
+                                  {10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4},
+                                  {3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14 } };
 
-int S5[4][16] = {{2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9 },
-                {14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6},
-                {4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14},
-                {11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3 }};
+const unsigned char S5[4][16] = {{2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9 },
+                                 {14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6},
+                                 {4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14},
+                                 {11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3 }};
 
-int S6[4][16] = { {12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11 },
-                {10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8},
-                {9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6},
-                {4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13}};
+const unsigned char S6[4][16] = { {12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11 },
+                                  {10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8},
+                                  {9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6},
+                                  {4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13}};
 
-int S7[4][16] = { { 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1},
-                {13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6},
-                {1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2},
-                {6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12} };
+const unsigned char S7[4][16] = { { 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1},
+                                  {13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6},
+                                  {1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2},
+                                  {6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12} };
 
-int S8[4][16] = { {13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7},
-                {1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2 },
-                {7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8},
-                {2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11 }};
+const unsigned char S8[4][16] = { {13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7},
+                                  {1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2 },
+                                  {7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8},
+                                  {2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11 }};
 
 
-int initial_message_permutation[] =	   {58, 50, 42, 34, 26, 18, 10, 2,
-										60, 52, 44, 36, 28, 20, 12, 4,
-										62, 54, 46, 38, 30, 22, 14, 6,
-										64, 56, 48, 40, 32, 24, 16, 8,
-										57, 49, 41, 33, 25, 17,  9, 1,
-										59, 51, 43, 35, 27, 19, 11, 3,
-										61, 53, 45, 37, 29, 21, 13, 5,
-										63, 55, 47, 39, 31, 23, 15, 7};
+const unsigned char initial_message_permutation[] =	   {58, 50, 42, 34, 26, 18, 10, 2,
+										                60, 52, 44, 36, 28, 20, 12, 4,
+										                62, 54, 46, 38, 30, 22, 14, 6,
+										                64, 56, 48, 40, 32, 24, 16, 8,
+										                57, 49, 41, 33, 25, 17,  9, 1,
+										                59, 51, 43, 35, 27, 19, 11, 3,
+										                61, 53, 45, 37, 29, 21, 13, 5,
+										                63, 55, 47, 39, 31, 23, 15, 7};
 
-int final_message_permutation[] =  {40,  8, 48, 16, 56, 24, 64, 32,
-									39,  7, 47, 15, 55, 23, 63, 31,
-									38,  6, 46, 14, 54, 22, 62, 30,
-									37,  5, 45, 13, 53, 21, 61, 29,
-									36,  4, 44, 12, 52, 20, 60, 28,
-									35,  3, 43, 11, 51, 19, 59, 27,
-									34,  2, 42, 10, 50, 18, 58, 26,
-									33,  1, 41,  9, 49, 17, 57, 25};
+const unsigned char final_message_permutation[] =  {40,  8, 48, 16, 56, 24, 64, 32,
+									                39,  7, 47, 15, 55, 23, 63, 31,
+									                38,  6, 46, 14, 54, 22, 62, 30,
+									                37,  5, 45, 13, 53, 21, 61, 29,
+									                36,  4, 44, 12, 52, 20, 60, 28,
+									                35,  3, 43, 11, 51, 19, 59, 27,
+									                34,  2, 42, 10, 50, 18, 58, 26,
+									                33,  1, 41,  9, 49, 17, 57, 25};
 
-int message_expansion[] =  {32,  1,  2,  3,  4,  5,
-							 4,  5,  6,  7,  8,  9,
-							 8,  9, 10, 11, 12, 13,
-							12, 13, 14, 15, 16, 17,
-							16, 17, 18, 19, 20, 21,
-							20, 21, 22, 23, 24, 25,
-							24, 25, 26, 27, 28, 29,
-							28, 29, 30, 31, 32,  1};
+const unsigned char message_expansion[] =  {32,  1,  2,  3,  4,  5,
+							                 4,  5,  6,  7,  8,  9,
+							                 8,  9, 10, 11, 12, 13,
+							                12, 13, 14, 15, 16, 17,
+							                16, 17, 18, 19, 20, 21,
+							                20, 21, 22, 23, 24, 25,
+							                24, 25, 26, 27, 28, 29,
+							                28, 29, 30, 31, 32,  1};
 
-unsigned char keyPC1[56] = {
-   57, 49, 41, 33, 25, 17,  9,
-    1, 58, 50, 42, 34, 26, 18,
-   10,  2, 59, 51, 43, 35, 27,
-   19, 11,  3, 60, 52, 44, 36,
-   63, 55, 47, 39, 31, 23, 15,
-    7, 62, 54, 46, 38, 30, 22,
-   14,  6, 61, 53, 45, 37, 29,
-   21, 13,  5, 28, 20, 12,  4
-    };
+const unsigned char keyPC1[56] = { 57, 49, 41, 33, 25, 17,  9,
+                                    1, 58, 50, 42, 34, 26, 18,
+                                   10,  2, 59, 51, 43, 35, 27,
+                                   19, 11,  3, 60, 52, 44, 36,
+                                   63, 55, 47, 39, 31, 23, 15,
+                                    7, 62, 54, 46, 38, 30, 22,
+                                   14,  6, 61, 53, 45, 37, 29,
+                                   21, 13,  5, 28, 20, 12,  4 };
 
-unsigned char keyPC2[48] = { 14, 17, 11, 24, 1,  5,  3,  28,
-                         15, 6,  21, 10, 23, 19, 12, 4,
-                         26, 8,  16, 7,  27, 20, 13, 2,
-                         41, 52, 31, 37, 47, 55, 30, 40,
-                         51, 45, 33, 48, 44, 49, 39, 56,
-                         34, 53, 46, 42, 50, 36, 29, 32 };
-unsigned long long PermutedChoice1(unsigned long long key)//Amin
+const unsigned char keyPC2[48] = { 14, 17, 11, 24, 1,  5,  3,  28,
+                                   15, 6,  21, 10, 23, 19, 12, 4,
+                                   26, 8,  16, 7,  27, 20, 13, 2,
+                                   41, 52, 31, 37, 47, 55, 30, 40,
+                                   51, 45, 33, 48, 44, 49, 39, 56,
+                                   34, 53, 46, 42, 50, 36, 29, 32 };
+
+unsigned long long PermutedChoice1(unsigned long long key)
 {
     unsigned long long PC1 = 0;
     
@@ -106,11 +108,6 @@ unsigned long long PermutedChoice1(unsigned long long key)//Amin
     }
     return PC1;
 }
-
-
-
-
-
 
 unsigned long long PermutedChoice2(unsigned long long key)
 {
@@ -238,26 +235,26 @@ long S8_API(long S8_InputData)
 unsigned long long SBox(unsigned long long text)
 {
     long long output;
-    int TotalInputSize = 48;
-    int TotalOutputSize = 32;
-    int BoxInputSize = 6;
-    int BoxOutputSize = 4;
-    int S1_Offset = 42;
-    int S2_Offset = 36;
-    int S3_Offset = 30;
-    int S4_Offset = 24; 
-    int S5_Offset = 18;
-    int S6_Offset = 12;
-    int S7_Offset = 6;
-    int S8_Offset = 0;
-    int S1_Output_Offset = 28;
-    int S2_Output_Offset = 24;
-    int S3_Output_Offset = 20;
-    int S4_Output_Offset = 16;
-    int S5_Output_Offset = 12;
-    int S6_Output_Offset = 8;
-    int S7_Output_Offset = 4;
-    int S8_Output_Offset = 0;
+    static const int TotalInputSize = 48;
+    static const int TotalOutputSize = 32;
+    static const int BoxInputSize = 6;
+    static const int BoxOutputSize = 4;
+    static const int S1_Offset = 42;
+    static const int S2_Offset = 36;
+    static const int S3_Offset = 30;
+    static const int S4_Offset = 24; 
+    static const int S5_Offset = 18;
+    static const int S6_Offset = 12;
+    static const int S7_Offset = 6;
+    static const int S8_Offset = 0;
+    static const int S1_Output_Offset = 28;
+    static const int S2_Output_Offset = 24;
+    static const int S3_Output_Offset = 20;
+    static const int S4_Output_Offset = 16;
+    static const int S5_Output_Offset = 12;
+    static const int S6_Output_Offset = 8;
+    static const int S7_Output_Offset = 4;
+    static const int S8_Output_Offset = 0;
     
 
     long S1_InputData = (text & (0b111111000000000000000000000000000000000000000000)) >> S1_Offset;
@@ -275,20 +272,6 @@ unsigned long long SBox(unsigned long long text)
     output = (S8_API(S8_InputData) << S8_Output_Offset) | ((S7_API(S7_InputData) << S7_Output_Offset)) | ((S6_API(S6_InputData) << S6_Output_Offset)) | (S5_API(S5_InputData) << S5_Output_Offset) | ((S4_API(S4_InputData) << S4_Output_Offset)) | ((S3_API(S3_InputData) << S3_Output_Offset)) | ((S2_API(S2_InputData) << S2_Output_Offset)) | ((S1_API(S1_InputData) << S1_Output_Offset));
 
     return output;
-}
-
-unsigned long long ascii_to_hex(char c)
-{
-        unsigned long long  num = (int) c;
-        if(num < 58 && num > 47)
-        {
-                return num - 48; 
-        }
-        if(num < 103 && num > 96)
-        {
-                return num - 87;
-        }
-        return num;
 }
 
 unsigned long long Permutation(unsigned long long text)
@@ -352,10 +335,6 @@ unsigned long long decrypt(unsigned long long cipher)
     return InverseInitialPermutation(text);
 }
 
-unsigned long long textBlocks[16][1024];
-unsigned long long cipherBlocks[16][1024];
-char cipherHex[16][17*1024 + 1];
-
 unsigned long long IHateEndinanness(unsigned long long text)
 {
     text = (text << 32) | (text >> 32);
@@ -363,6 +342,10 @@ unsigned long long IHateEndinanness(unsigned long long text)
     text = ((text << 8) & 0xFF00FF00FF00FF00) | ((text >> 8) & 0x00FF00FF00FF00FF);
     return text;
 }
+
+unsigned long long textBlocks[THREAD_COUNT][BLOCK_SIZE];
+unsigned long long cipherBlocks[THREAD_COUNT][BLOCK_SIZE];
+char cipherHex[THREAD_COUNT][17*BLOCK_SIZE + 1];
 
 typedef struct {
     char ThreadNumber;
@@ -400,6 +383,9 @@ void * decryptBlock(void *args)
     return NULL;
 }
 
+clock_t FileIO_time = 0;
+clock_t Algorithm_time = 0; 
+
 int main(int argc, char* argv[])
 {
     clock_t start = clock();
@@ -434,35 +420,43 @@ int main(int argc, char* argv[])
             getchar();
             return 0;
         }
-        size_t blockCount = fread((void*) textBlocks, sizeof(unsigned long long), 16*1024, textFile);
+        size_t blockCount = fread((void*) textBlocks, sizeof(unsigned long long), THREAD_COUNT*BLOCK_SIZE, textFile);
         while (blockCount > 0) {
-            int maximum = blockCount < 16*1024? blockCount:16*1024;
+            clock_t pre_start = clock();
+            int maximum = blockCount < THREAD_COUNT*BLOCK_SIZE? blockCount:THREAD_COUNT*BLOCK_SIZE;
             int remaining = maximum;
-            pthread_t threads[16];
-            ThreadArgs args[16];
+            pthread_t threads[THREAD_COUNT];
+            ThreadArgs args[THREAD_COUNT];
             int threadCount = 0;
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < THREAD_COUNT; i++)
             {
                 args[i].ThreadNumber = i;
-                args[i].maximum = remaining < 1024? remaining:1024;
+                args[i].maximum = remaining < BLOCK_SIZE? remaining:BLOCK_SIZE;
                 pthread_create(&threads[i], NULL, &encryptBlock, (void *)&args[i]);
                 remaining -= args[i].maximum;
                 threadCount++;
                 if (remaining == 0) break;
             }
-
+            clock_t pre_end = clock();
+            FileIO_time += pre_end - pre_start;
+            clock_t algo_start = clock();
             for (int i = 0; i < threadCount; i++)
             {
                 pthread_join(threads[i], NULL);
             }
+            clock_t algo_end = clock();
+            Algorithm_time += algo_end - algo_start;
 
+            clock_t post_start = clock();
             fwrite((const void*) cipherBlocks, sizeof(unsigned long long), maximum, cipherFile);
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < THREAD_COUNT; i++)
             {
                 fputs(cipherHex[i], cipherHexFile);
             }
             
-            blockCount = fread((void*) textBlocks, sizeof(unsigned long long), 16*1024, textFile);
+            blockCount = fread((void*) textBlocks, sizeof(unsigned long long), THREAD_COUNT*BLOCK_SIZE, textFile);
+            clock_t post_end = clock();
+            FileIO_time += post_end - post_start;
         }
         fclose(keyFile);
         fclose(textFile);
@@ -500,17 +494,17 @@ int main(int argc, char* argv[])
             getchar();
             return 0;
         }
-        size_t blockCount = fread((void*) cipherBlocks, sizeof(unsigned long long), 16*1024, cipherFile);
+        size_t blockCount = fread((void*) cipherBlocks, sizeof(unsigned long long), THREAD_COUNT*BLOCK_SIZE, cipherFile);
         while (blockCount > 0) {
-            int maximum = blockCount < 16*1024? blockCount:16*1024;
+            int maximum = blockCount < THREAD_COUNT*BLOCK_SIZE? blockCount:THREAD_COUNT*BLOCK_SIZE;
             int remaining = maximum;
-            pthread_t threads[16];
-            ThreadArgs args[16];
+            pthread_t threads[THREAD_COUNT];
+            ThreadArgs args[THREAD_COUNT];
             int threadCount = 0;
-            for (int i = 0; i < 16; i++)
+            for (int i = 0; i < THREAD_COUNT; i++)
             {
                 args[i].ThreadNumber = i;
-                args[i].maximum = remaining < 1024? remaining:1024;
+                args[i].maximum = remaining < BLOCK_SIZE? remaining:BLOCK_SIZE;
                 pthread_create(&threads[i], NULL, &decryptBlock, (void *)&args[i]);
                 remaining -= args[i].maximum;
                 threadCount++;
@@ -523,7 +517,7 @@ int main(int argc, char* argv[])
             }
 
             fwrite((const void*) textBlocks, sizeof(unsigned long long), maximum, textFile);
-            blockCount = fread((void*) cipherBlocks, sizeof(unsigned long long), 16*1024, cipherFile);
+            blockCount = fread((void*) cipherBlocks, sizeof(unsigned long long), THREAD_COUNT*BLOCK_SIZE, cipherFile);
         }
         fclose(textFile);
         fclose(cipherFile);
@@ -532,6 +526,8 @@ int main(int argc, char* argv[])
     else printf("Invalid Arguments\n");
     clock_t end = clock();
     printf("Process took %f seconds\n", (float)(end-start)/CLOCKS_PER_SEC);
+    printf("File IO took %f seconds\n", (float)(FileIO_time)/CLOCKS_PER_SEC);
+    printf("Algorithm took %f seconds\n", (float)(Algorithm_time)/CLOCKS_PER_SEC);
     printf("Press Enter to Continue..");
     getchar();
     return 0;
